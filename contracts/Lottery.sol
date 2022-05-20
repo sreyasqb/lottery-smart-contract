cd805f96f882b8b357ebc0b6c3ed0652fed5e8fe
sreyasqb <20pt33@psgtech.ac.in>
2022-05-20 17:23:36 +0530

    updated lottery.sol

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,34 +1,31 @@
-pragma solidity ^0.4.17;
-
-contract Lottery {
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+contract Inbox{
     address public manager;
     address[] public players;
-    
-    function Lottery() public {
-        manager = msg.sender;
+    constructor () {
+        manager=msg.sender;
     }
-    
-    function enter() public payable {
-        require(msg.value > .01 ether);
+    function addPlayer() public payable {
+        require(msg.value>0.1 ether); // the value sending has to be more than 0.1 ether
         players.push(msg.sender);
     }
     
-    function random() private view returns (uint) {
-        return uint(keccak256(block.difficulty, now, players));
+    function random() private view returns (uint){
+        return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,players))); //generates the random algorithm
     }
-    
-    function pickWinner() public restricted {
-        uint index = random() % players.length;
-        players[index].transfer(this.balance);
+    function getWinner() public payable restricted{
+        
+        uint index=random()%players.length;
+        payable(players[index]).transfer(address(this).balance); //transfering the winner the balance
         players = new address[](0);
     }
-    
-    modifier restricted() {
-        require(msg.sender == manager);
+    function getPlayers() public view restricted returns(address[] memory){
+        return players; //the restricted keyword is a modifier that only manager can call it
+    }
+    modifier restricted(){ //the modifier
+        require(msg.sender==manager);
         _;
     }
     
-    function getPlayers() public view returns (address[]) {
-        return players;
-    }
-}   
+}
