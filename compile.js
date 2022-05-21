f8a957d1cef15479d1c5823781b966544495d311
sreyasqb <20pt33@psgtech.ac.in>
2022-05-21 13:23:15 +0530

    switching to solidity 0.8.13 finally it works

--- compile.js
+++ compile.js
@@ -1,8 +1,29 @@
-const path = require("path");
-const fs = require("fs");
-const solc = require("solc");
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
 
-const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
-const source = fs.readFileSync(lotteryPath, "utf8");
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
+const source = fs.readFileSync(lotteryPath, 'utf8');
 
-module.exports = solc.compile(source, 1).contracts[":Lottery"];
+const input = {
+  language: 'Solidity',
+  sources: {
+    'Lottery.sol': {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*'],
+      },
+    },
+  },
+};
+
+// console.log(JSON.parse(solc.compile(JSON.stringify(input))).contracts['Lottery.sol'].Inbox);
+
+var output=JSON.parse(solc.compile(JSON.stringify(input))).contracts['Lottery.sol'].Inbox;
+module.exports.abi=output.abi;
+module.exports.bytecode=output.evm.bytecode.object;
+
